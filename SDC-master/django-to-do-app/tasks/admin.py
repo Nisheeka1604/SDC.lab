a98b1d1dae4ffeea46b8a6606cc34c05f991374c
Nisheeka1604 <nisheekapudota@gmail.com>
2025-06-10 22:42:26 +0530

    newsdc

--- SDC-master/django-to-do-app/tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from tasks.models import Task
-
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'is_completed', 'is_archived', 'is_deleted',)
-    ordering = ('id',)
-
-admin.site.register(Task, TaskAdmin)
